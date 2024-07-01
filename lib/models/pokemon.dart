e015df0aa742609c473d3abc86edb73ab751e773
Jean Carlo <vaniapolo@gmail.com>
2024-07-01 11:18:16 -0300

    pokemon model

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,33 +1,33 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:pokedex/constants/pokemon_type.dart';
 
+import 'package:pokedex/constants/pokemon_type.dart';
 
 class Pokemon {
   int id;
   String name;
   List<PokemonType> pokemonTypes = [];
-  Image image;
+  String sprite;
+
   Pokemon({
     required this.id,
     required this.name,
     required this.pokemonTypes,
-    required this.image,
+    required this.sprite,
   });
 
   Pokemon copyWith({
     int? id,
     String? name,
     List<PokemonType>? pokemonTypes,
-    Image? image,
+    String? sprite,
   }) {
     return Pokemon(
       id: id ?? this.id,
       name: name ?? this.name,
       pokemonTypes: pokemonTypes ?? this.pokemonTypes,
-      image: image ?? this.image,
+      sprite: sprite ?? this.sprite,
     );
   }
 
@@ -36,7 +36,7 @@ class Pokemon {
       'id': id,
       'name': name,
       'pokemonTypes': pokemonTypes.map((x) => x.name).toList(),
-      'image': image.toMap(),
+      'sprite': sprite,
     };
   }
 
@@ -44,36 +44,36 @@ class Pokemon {
     return Pokemon(
       id: map['id'] as int,
       name: map['name'] as String,
-      pokemonTypes: (map['types'] as List<String>).map((typeName) => PokemonType.fromString(typeName)!).toList()
-      image: Image.fromMap(map['image'] as Map<String,dynamic>),
+      pokemonTypes: (map['types'] as List).map((type) => PokemonType.fromString(type['name'])!).toList(),
+      sprite: map['sprites']['front_default'] 
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Pokemon.fromJson(String source) => Pokemon.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory Pokemon.fromJson(String source) =>
+      Pokemon.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
-    return 'Pokemon(id: $id, name: $name, pokemonTypes: $pokemonTypes, image: $image)';
+    return 'Pokemon(id: $id, name: $name, pokemonTypes: $pokemonTypes, sprite: $sprite)';
   }
 
   @override
   bool operator ==(covariant Pokemon other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.id == id &&
-      other.name == name &&
-      listEquals(other.pokemonTypes, pokemonTypes) &&
-      other.image == image;
+
+    return other.id == id &&
+        other.name == name &&
+        listEquals(other.pokemonTypes, pokemonTypes) &&
+        other.sprite == sprite;
   }
 
   @override
   int get hashCode {
     return id.hashCode ^
-      name.hashCode ^
-      pokemonTypes.hashCode ^
-      image.hashCode;
+        name.hashCode ^
+        pokemonTypes.hashCode ^
+        sprite.hashCode;
   }
 }
