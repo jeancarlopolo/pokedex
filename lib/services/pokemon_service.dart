82c60727b27df277941b03c8e656242424f7c01c
Jean Carlo <vaniapolo@gmail.com>
2024-07-02 14:27:35 -0300

    filtro e pesquisa

--- lib/services/pokemon_service.dart
+++ lib/services/pokemon_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:pokedex/constants/pokemon_type.dart';
 import 'package:pokedex/models/pokemon.dart';
 import 'package:signals/signals.dart';
 
@@ -12,6 +13,11 @@ class PokemonService {
   List<Pokemon> get pokemons => _pokemons;
   Signal<bool> get isLoading => _isLoading;
 
+  void clear() {
+    _page = 0;
+    _pokemons.clear();
+  }
+
   Future<void> fetchPokemons() async {
     if (_isLoading.value) return;
 
@@ -21,9 +27,57 @@ class PokemonService {
         'https://pokeapi.co/api/v2/pokemon?offset=${_page * 20}&limit=20'));
     final data = json.decode(response.body);
 
-    _pokemons.addAll((data['results'] as List)
-        .map((json) => Pokemon.fromJson(json))
-        .toList());
+    for (Map pokemon in data['results'] as List) {
+      final pokemonResponse = await http.get(Uri.parse(pokemon['url']));
+      final pokemonData = json.decode(pokemonResponse.body);
+      _pokemons.add(Pokemon.fromJson(pokemonData));
+    }
+
+    _page++;
+
+    _isLoading.value = false;
+  }
+
+  Future<void> fetchPokemonsByType(PokemonType type) async {
+    if (_isLoading.value) return;
+
+    _isLoading.value = true;
+    clear();
+
+    final response = await http
+        .get(Uri.parse('https://pokeapi.co/api/v2/type/${type.name}'));
+    final data = json.decode(response.body);
+
+    for (Map pokemonMap in data['pokemon'] as List) {
+      final pokemonResponse =
+          await http.get(Uri.parse(pokemonMap['pokemon']['url']));
+      final pokemonData = json.decode(pokemonResponse.body);
+      _pokemons.add(Pokemon.fromJson(pokemonData));
+    }
+    _page++;
+
+    _isLoading.value = false;
+  }
+
+  Future<void> fetchPokemonsByName(String name) async {
+    if (_isLoading.value) return;
+
+    _isLoading.value = true;
+
+    final response = await http
+        .get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=10000'));
+    final data = json.decode(response.body);
+
+    for (Map pokemon in data['results'] as List) {
+      if ((pokemon['name'] as String)
+          .toLowerCase()
+          .contains(name.toLowerCase().trim())) {
+        final pokemonResponse = await http.get(Uri.parse(pokemon['url']));
+        final pokemonData = json.decode(pokemonResponse.body);
+        _pokemons.add(Pokemon.fromJson(pokemonData));
+      }
+    }
+
     _page++;
 
     _isLoading.value = false;
